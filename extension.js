e8df99141767d1b40c5a1ce751967c4deda41491
Richard01CZ <Richard.Budicek@email.cz>
2016-06-03 21:09:35 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Richard01CZ/plugBot/master/plugBot.js", extend);
+    $.getScript("https://rawgit.com/Richard01CZ/plugBot/master/basicBot.js", extend);
 
 }).call(this);
